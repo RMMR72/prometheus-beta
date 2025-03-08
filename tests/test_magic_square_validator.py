bc918cb1384eba063cf0a8eda9987bd1007b2443
RMMR72 <RMMR72@users.noreply.github.com>
2025-03-08 13:36:38 +0000

    Update test cases to clarify magic square validation

--- tests/test_magic_square_validator.py
+++ tests/test_magic_square_validator.py
@@ -26,6 +26,7 @@ def test_invalid_magic_square_duplicate_numbers():
 
 def test_non_magic_square_arrangement():
     """Test an arrangement that doesn't form a magic square"""
+    # This should be a sequence that uses 1-9 but doesn't sum to magic constant
     assert is_magic_square([1,2,3,4,5,6,7,8,9,0]) == False
 
 def test_edge_case_empty_list():
